72838ad30c019694f348acd0ec9e29c91fd99461
Garv <garvchawla01@gmail.com>
2026-06-07 02:08:13 +0530

    Lifting state up of cart

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import {HomePage} from './pages/HomePage'
 import { CheckOut } from './pages/CheckOut'
 import { OrderPage } from './pages/OrdersPage'
@@ -5,13 +6,21 @@ import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 import {Routes, Route} from 'react-router'
 import { PageNotFound } from './pages/PageNotFound'
+import { useEffect , useState} from 'react'
 
 function App() {
+    const [cart,setCart] = useState([])
 
+    useEffect(() =>{
+      axios.get('/api/cart-items')
+        .then( (response)=> {
+          setCart(response.data)
+        })
+    },[])
   return (
     <Routes>
       {/* <Route path='/' element={<HomePage/>}/> both are same */}
-      <Route index element={<HomePage/>}/>
+      <Route index element={<HomePage cart = {cart}/>}/>
       <Route path='checkout' element={<CheckOut/>}/>
       <Route path='orders' element={<OrderPage/>}/>
       <Route path='tracking' element={<TrackingPage/>}/>
--- e-commerce/src/pages/HomePage.jsx
+++ e-commerce/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { Header } from '../component/Header';
 // import {products} from '../../starting-code/data/products'
 import { useEffect, useState } from 'react';
 
-export function HomePage(){
+export function HomePage({cart}){
 
     // fetch('http://localhost:3000/api/products')
     // .then( (response) => {
@@ -13,17 +13,13 @@ export function HomePage(){
     //     } );  
     // })
     const [products,setProducts] = useState([])
-    const [cart,setCart] = useState([])
     useEffect(() => {
       axios.get('/api/products')
       .then( (response) => {
         setProducts(response.data)
         } ); 
       
-      axios.get('/api/cart-items')
-      .then( (response)=> {
-        setCart(response.data)
-      })
+      
     },[])
      
 
